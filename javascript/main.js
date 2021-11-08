805c74bb3cb7b7f38163b686d6a210caf492759a
Liel Itzchak <liel.izcha321@gmail.com>
2021-11-08 14:19:50 +0200

    add async function

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,33 @@
 const BASIC_API = "https://moviesmern.herokuapp.com";
-let allMovie = "/movies/all";
+const allMovie = "/movies/all";
 let footer = document.getElementById("footer");
+let main = document.getElementById("main");
 let divHeaderHomePage = document.getElementById("divHeaderHomePage");
+function showErrorImg() {
+  main.innerHTML = `<img id="errorImg500" src="../media/video/error 500.gif" alt="">`;
+}
+function getData(data) {
+  return new Promise((resolve, reject) => {
+    if (data < 600) {
+      resolve(data);
+    }
+    reject(showErrorImg());
+  });
+}
+async function movieFromApi(api, goTo, option) {
+  try {
+    loadionImg();
+    return await fetch(`${api}${goTo}`, option).then((res) => {
+      return res.json();
+    });
+  } catch {
+    return showErrorImg();
+  } finally {
+    return stopLoadionImg();
+  }
+}
 function loadionImg() {
-  `<img src="../media/video/daoling time gif.gif" alt="">`;
+  return `<img src="../media/video/daoling time gif.gif" alt="">`;
 }
 function stopLoadionImg() {}
 function addHeader(div) {
@@ -27,24 +51,23 @@ function addHeader(div) {
       </nav>
     </header>`;
 }
-addHeader(divHeaderHomePage);
 
-function showFooter() {
-  footer.innerHTML += `
-    <a class="linkToContent" href="https://did.li/Q5lTY"><img src="../media/photos/linkedin.png" alt="linkedin" class="imgsLogo"></a>
-    <a class="linkToContent" href="https://did.li/WNarl"><img src="../media/photos/github.png"" alt="github" class="imgsLogo"></a>
-    <a class="linkToContent" href="mailto:liel.izcha321@gmail.com"><img src="../media/photos/gmail.png" alt="gmail" class="imgsLogo"></a>
-    <a class="linkToContent" href=""><img src="../media/photos/whatsapp.png" alt="whatsApp" class="imgsLogo"></a>
+function showFooter(div) {
+  div.innerHTML += `
+    <section class="allLogos"> 
+        <a class="linkToContent" href="https://did.li/Q5lTY"><img src="../media/photos/linkedin.png" alt="linkedin" class="imgsLogo"></a>
+        <a class="linkToContent" href="https://did.li/WNarl"><img src="../media/photos/github.png"" alt="github" class="imgsLogo"></a>
+        <a class="linkToContent" href="mailto:liel.izcha321@gmail.com"><img src="../media/photos/gmail.png" alt="gmail" class="imgsLogo"></a>
+        <a class="linkToContent" href=""><img src="../media/photos/whatsapp.png" alt="whatsApp" class="imgsLogo"></a>
+    </section>
+    <section class="copyRight">  
+      <img class="copyRightImg" src="../media/photos/copyright.png" alt="whatsApp">
+      <p>liel itzchak</p>
+    </section>
+    
+  
     `;
 }
-showFooter();
+addHeader(divHeaderHomePage);
 
-async function movieFromApi(api, goTo, option) {
-  try {
-    return await fetch(`${api}${goTo}`, option).then((res) => {
-      return res.json();
-    });
-  } catch (error) {
-    return error;
-  }
-}
+showFooter(footer);
