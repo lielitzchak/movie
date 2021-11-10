f151e4509ec3d284dc14e08d99ea205505c96b27
Liel Itzchak <liel.izcha321@gmail.com>
2021-11-10 22:08:29 +0200

    add function

--- javascript/tabs.js
+++ javascript/tabs.js
@@ -2,37 +2,8 @@ let sectionToMoviesCart = document.getElementById("sectionToMoviesCart");
 let select = document.getElementById("select");
 let searchBtn = document.getElementById("searchBtn");
 let sortBy = document.getElementById("sortBy");
-
-sortBy.oninput = () => {
-  addOption();
-};
-function addOption() {
-  switch (select.value) {
-    case "sort by top rating":
-      listOfMovies.sort((a, b) => {
-        return b.rating - a.rating;
-      }),
-        aaa(listOfMovies);
-      console.log("sort by top rating");
-      return;
-
-    case "sort by name":
-      listOfMovies.sort((a, b) => {
-        if (a.movieName < b.movieName) return -1;
-      });
-      console.log(listOfMovies);
-      console.log("sort by name");
-      aaa(listOfMovies);
-      return;
-    case "sort by date":
-      return console.log("sort by date");
-  }
-
-  return;
-}
-
-getData(BASIC_API, allMovie).then((resolve) => {
-  sectionToMoviesCart.innerHTML = "";
+function showInWindow(resolve) {
+  sectionToMoviesCart.innerHTML += "";
   for (const movie of resolve.data) {
     sectionToMoviesCart.innerHTML += `
     <article class="moviesCart">
@@ -55,42 +26,35 @@ getData(BASIC_API, allMovie).then((resolve) => {
     </article>
 `;
   }
+}
+getData(BASIC_API, allMovie).then((resolve) => {
+  showInWindow(resolve);
 });
-//! /////////////////////////////////////////////////////////
-//! /////////////////////////////////////////////////////////
-//! /////////////////////////////////////////////////////////
-//! /////////////////////////////////////////////////////////
-//! /////////////////////////////////////////////////////////
-//! /////////////////////////////////////////////////////////
 
-// function showAllTabs() {
-//   getData(BASIC_API, allMovie).then((resolve) => {
-//     for (const movie of resolve.data) {
-//       sectionToMoviesCart.innerHTML += `
-//     <article class="moviesCart">
-//       <a href="../html/dataId.html?id=${movie._id}">
-//         <section class="img-container">
-//           <div class="img-inner">
-//               <div class="inner-skew">
-//                 <img class="imgTabs" src="${movie.image}" alt="">
-//               </div>
-//           </div>
-//         </section>
-//         <section class="text-container">
-//               <h3>${movie.movieName}</h3>
-//             <div>
-//               <p>${movie.rating}</p>
-//               <button id="${movie._id}" class="showDetails">see all details</button>
-//           </div>
-//         </section>
-//       </a>
-//     </article>
-// `;
-//     }
-//   });
-// }
+sortBy.oninput = () => {
+  addOption();
+};
 
-// showAllTabs();
-// let span = document.getElementsByClassName("close")[0];
-// let showDetails = document.getElementById("showDetails");
-// function showPopUp() {}
+function addOption() {
+  switch (select.value) {
+    case "sort by top rating":
+      getData(BASIC_API, allMovie).then((response) => {
+        // console.log(response.data);
+        response.data.sort((a, b) => {
+          return b.rating - a.rating;
+        });
+      });
+      console.log("sort by top rating");
+      return;
+    case "sort by name":
+      getData(BASIC_API, allMovie).then((response) => {
+        response.data.sort((a, b) => {
+          if (a.movieName < b.movieName) return -1;
+        });
+      });
+      console.log("sort by name");
+      return;
+    case "sort by date":
+      return console.log("sort by date");
+  }
+}
